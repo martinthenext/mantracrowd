dbcc935320dfd41da710fb0a3ebd91cbd6055614
martinthenext <martin.davtyan@gmail.com>
2013-08-20 14:36:32 +0200

    Polished URL structure

--- urls.py
+++ urls.py
@@ -1,10 +1,19 @@
 from django.conf.urls.defaults import patterns, include, url
+from django.http import HttpResponseRedirect
 
-# Uncomment the next two lines to enable the admin:
+import settings
+
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
+  url(r'^$', lambda r: HttpResponseRedirect(settings.STATIC_URL + 'disambiguation.html')),
+
   url(r'^accounts/reg/$', 'accounts.views.register_view'),
+  url(r'^accounts/login/$', 'accounts.views.login_view'),
+  url(r'^accounts/logout/$', 'accounts.views.logout_view'),
+
+  url(r'^disambig/next/$', 'disambig.views.next_question'),
+  
   url(r'^admin/', include(admin.site.urls)),
 )
